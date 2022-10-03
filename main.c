9f998905fdf0402ec40c1d7de13b255eead52b44
Andrew <nullsurface@github.com>
2022-10-02 22:32:18 -0700

    Represented roman numberals

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+char numerals[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+
+int num_digits(int num);
 
 int main () {
-    printf("Hello World!\n");
+    int num = 4231;
+    int digits = num_digits(num);
+    char* roman = (char*)malloc(digits);
+    
+    for (int i = 0; i < digits; i++) {}
 
     return 0;
+}
+
+int num_digits(int num) {
+    if (num > 10) 
+        return 1 + num_digits(num / 10);
+    else 
+        return 1;
 }
