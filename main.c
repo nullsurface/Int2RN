a38f016ae706823bea3ac03b4b6bb9b383c4cec1
Andrew <nullsurface@github.com>
2022-10-03 21:42:33 -0700

    Got 1-3 working

--- main.c
+++ main.c
@@ -5,17 +5,45 @@ char numerals[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
 int num_digits(int num);
 
 int main () {
-    int num = 4231;
+    int num = 2;
     int digits = num_digits(num);
-    char* roman = (char*)malloc(digits);
+    char* roman = (char*)malloc(sizeof(char) * 10);
+    int curr_digit;
+    int last_pos = 0;
     
-    for (int i = 0; i < digits; i++) {}
+    for (int i = 0; i < digits; i++) {
+        curr_digit = num % 10;
+        num /= 10;
+        
+        switch (curr_digit) {
+            case 1 ... 3:
+                switch (i) {
+                    case 0:
+                        for (int count = 0; count < curr_digit; count++) {
+                            roman[last_pos + count] = (char)numerals[i * 2];
+                        }
+                        break;
+                    case 2 ... 3:
+                        break;
+                }
+                break;
+            case 4:
+                break;
+            case 5 ... 8:
+                break;
+            case 9:
+                break;
+        }
+    }
+    
+    roman[9] = '\0';
+    printf("%s", roman);
 
     return 0;
 }
 
 int num_digits(int num) {
-    if (num > 10) 
+    if (num >= 10) 
         return 1 + num_digits(num / 10);
     else 
         return 1;
