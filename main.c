80287495cc285aea30cf71cf9d07deed82b4f90e
Andrew <nullsurface@github.com>
2022-10-04 22:06:31 -0700

    Worked on 4-5

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 char numerals[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
 
-int num_digits(int num);
-
+int num_digits(int);
+int add_numerals(int, char*, int, int*);
 int main () {
-    int num = 12;
+    int num = 5; 
     int digits = num_digits(num);
     char* roman = (char*)malloc(sizeof(char) * 10);
     printf("%d\n", digits);
@@ -21,24 +21,18 @@ int main () {
                 printf("%d case 1 ... 3\n", i);
                 switch (i) {
                     case 0:
-                        for (int count = 0; count < curr_digit; count++) {
-                            printf("%d %c\n", count, roman[i+2]);
-                            roman[last_pos] = (char)numerals[i * 2];
-                            last_pos++;
-                        }
+                        add_numerals(curr_digit, roman, i, &last_pos);
                         break;
                     case 1 ... 3:
-                        printf("%d %c", curr_digit, roman[i+2]);
-                        for (int count = 0; count < curr_digit; count++) {
-                            roman[last_pos] = (char)numerals[i * 2];
-                            last_pos++;
-                        }
+                        add_numerals(curr_digit, roman, i, &last_pos); // need to fix tmr
                         break;
                 }
                 break;
             case 4:
                 break;
             case 5 ... 8:
+                printf("Case 5 ... 8:\n");
+                add_numerals(curr_digit, roman, i+1, &last_pos);
                 break;
             case 9:
                 break;
@@ -46,8 +40,8 @@ int main () {
         
     }
     
-    roman[9] = '\0';
-    printf("%s", roman);
+    roman[last_pos] = '\0';
+    printf("%s/n", roman);
 
     return 0;
 }
@@ -57,4 +51,12 @@ int num_digits(int num) {
         return 1 + num_digits(num / 10);
     else 
         return 1;
+}
+
+int add_numerals(int curr_digit, char* roman, int i, int* last_pos) {
+    printf("%d %c\n", curr_digit, roman[i+2]);
+    for (int count = 0; count < curr_digit; count++) {
+        roman[*last_pos] = (char)numerals[i * 2];
+        *last_pos = *last_pos + 1;
+    }
 }
